d00e1dacda9aac93f7d362dfd32d52a64bb238ac
KOOl1BoY <yashodha1998@gmail.com>
2025-09-16 16:28:04 +0530

    yeee

--- prgaf.c
+++ prgaf.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello, World!");
+    int a,b,c;
+    printf("Enter the values of a,b,c:");   
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b && a>c)
+        printf("a is the largest number");
+    else if(b>a && b>c)
+        printf("b is the largest number");
+    else
+        printf("c is the largest number");  
     return 0;
 }
